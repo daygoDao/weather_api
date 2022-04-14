a81ee026ddde31f6f8d5f427dcc26fe241ce9302
dao vo <gary.vo32@gmail.com>
2022-04-13 17:38:16 -0700

    yek ipa

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const locationName = document.querySelector("h2");
 const getWeather = async () => {
   try {
     const cityName = document.querySelector("#city").value;
-    const api_key = "ddb60140d329c8a98208e738298a1b19";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},us&appid=${api_key}&units=imperial`;
+    const yek_ipa = "ddb60140d329c8a98208e738298a1b19";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},us&appid=${yek_ipa}&units=imperial`;
     const res = await fetch(url);
     const weather = await res.json();
 
     outputDOM(weather);
     console.log(weather);
-    
+
   } catch (err) {
     console.log(err);
   }
